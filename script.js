de95c2bfb07506c32f5695d80ed6901d04424add
Fabrizio D'Isidoro <fabrizioandres98@gmail.com>
2023-10-04 00:10:16 -0300

    feat(script): codigo funcionando, falta pulir

--- script.js
+++ script.js
@@ -1,12 +1,61 @@
-const caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-let contraseña = " "
 
-function generarContraseña(largo) {
-    for (let i = 0; i<largo; i++) {
-        let indice = Math.floor(Math.random()*caracteres.length)
-        let caracterDelIndice = caracteres.charAt(indice)
-        contraseña += caracterDelIndice
+//CARACTERES PARA ARMAR CONTRASEÑA
+let longitudInput = " "
+const minusculas = "abcdefghijklmnopqrstuvwxyz"
+const mayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const numeros = "0123456789"
+const caracteresEspeciales = "!@#$%^&*()-_+=<>?/"
+let caracteresMarcados = ""
+//CARACTERES PARA ARMAR CONTRASEÑA
+let contraseña = ""
+//CONSTANTES DE LAS CHECKBOX TRAIDAS CON ID A JS
+const minusculasCheckbox = document.getElementById("minusculasCheckbox")
+const mayusculasCheckbox = document.getElementById("mayusculasCheckbox")
+const numerosCheckbox = document.getElementById("numerosCheckbox")
+const caracteresEspecialesCheckbox = document.getElementById("caracteresEspecialesCheckbox")
+//CONSTANTES DE LAS CHECKBOX TRAIDAS CON ID A JS
+
+const generarBtn = document.getElementById("generarBtn")
+
+//FUNCION QUE HACE QUE SE AGREGUE O SE QUITE EL STRING DEL CHECKBOX
+function chequear() {
+    if (minusculasCheckbox.checked) {
+        caracteresMarcados += minusculas
+    }
+    if (mayusculasCheckbox.checked) {
+        caracteresMarcados += mayusculas
+    }
+    if (numerosCheckbox.checked) {
+        caracteresMarcados += numeros
+    }
+    if (caracteresEspecialesCheckbox.checked) {
+        caracteresMarcados += caracteresEspeciales
+    }
+}
+//FUNCION QUE HACE QUE SE AGREGUE O SE QUITE EL STRING DEL CHECKBOX
+
+//EVENTOS
+minusculasCheckbox.addEventListener("change", chequear)
+mayusculasCheckbox.addEventListener("change", chequear)
+numerosCheckbox.addEventListener("change", chequear)
+caracteresEspecialesCheckbox.addEventListener("change", chequear)
+generarBtn.addEventListener("click", function() {
+    generarContraseña();
+    alert(contraseña);
+});
+//EVENTOS
+
+//FUNCION PARA ARMAR LA CONTRASEÑA
+function generarContraseña() {
+    contraseña = " "
+    longitudInput = document.getElementById("longitudInput").value
+    for (let i = 0; i < longitudInput; i++) {
+        let indice = Math.floor(Math.random() * caracteresMarcados.length)
+        let caracter = caracteresMarcados.charAt(indice)
+        contraseña += caracter
     }
     return contraseña
 }
-console.log(generarContraseña(20))
+//FUNCION PARA ARMAR LA CONTRASEÑA
+
+generarBtn.onclick = generarContraseña()
